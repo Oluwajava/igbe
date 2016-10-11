79ec6778dfd62328121da5f9dddd162b5ea6e01c
Mayokun Ige <oionetworks@gmail.com>
2016-10-11 10:40:59 +0100

    SplashScreen that explains what the application is about like that of Yahoo Digest was used to substitute normal splash screen

---
+++ app/src/main/java/com/snews/pison/snews/PrefManager.java
@@ -0,0 +1,38 @@
+package com.snews.pison.snews;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Olusegun Olaosebikan on 10/11/2016.
+ */
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context context;
+
+    int PRIVATE = 0;
+
+//    Share preferences file name
+    private static final String PREF_NAME = "snews_welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this.context = context;
+
+//        creates shared preference
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
--- app/src/main/java/com/snews/pison/snews/Selection.java
+++ app/src/main/java/com/snews/pison/snews/Selection.java
@@ -3,7 +3,7 @@ package com.snews.pison.snews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Selection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
