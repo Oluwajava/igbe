cba73508477bb6fc4353cec332602a5cab411f68
Mayokun Ige <ige@systemspecs.om.ng>
2017-01-18 20:21:25 +0100

    Merge remote-tracking branch 'remotes/origin/search_screen'
    # Conflicts:
    #	app/src/main/java/com/snews/pison/snews/fragments/FragmentForYou.java
    #	app/src/main/java/com/snews/pison/snews/fragments/FragmentSaved.java
    #	app/src/main/java/com/snews/pison/snews/fragments/FragmentSearch.java

--- app/src/main/java/com/snews/pison/snews/fragments/FragmentExplore.java
+++ app/src/main/java/com/snews/pison/snews/fragments/FragmentExplore.java
@@ -1,7 +1,14 @@
 package com.snews.pison.snews.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -20,6 +27,9 @@ import butterknife.ButterKnife;
  */
 public class FragmentExplore extends Fragment {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
     /** Required empty constructor */
     public FragmentExplore() {}
 
@@ -36,13 +46,93 @@ public class FragmentExplore extends Fragment {
         // Setup the toolbar
         Toolbar toolbar = ButterKnife.findById(rootView, R.id.toolbar);
         TextView toolBarTitle = ButterKnife.findById(toolbar, R.id.toolbar_title);
+
+        // Swipe tab initialization
+        tabLayout = (TabLayout) rootView.findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) rootView.findViewById(R.id.pager);
+
         toolBarTitle.setText(R.string.explore_tab);
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.setSupportActionBar(toolbar);
         activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        // Setup swipe view
+        viewPager.setAdapter(new FragmentExplore.CustomTabAdapter(getActivity().getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+//                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+//                viewPager.setCurrentItem(tab.getPosition());
+            }
+        });
+
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        };
+
         return rootView;
     }
 
+    private class CustomTabAdapter extends FragmentStatePagerAdapter {
+
+        String[] headers = {"TRENDING", "WORLD", "POLITICS"};
+        public CustomTabAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            Fragment frag = null;
+
+            switch (position) {
+                case 0:
+                    frag = new FragmentForYou();
+                    break;
+                case 1:
+                    frag = new FragmentForYou();
+                    break;
+                case 2:
+                    frag = new FragmentForYou();
+                    break;
+            }
+            return frag;
+        }
+
+        @Override
+        public int getCount() {
+            return headers.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return headers[position];
+        }
+    }
 
 }
